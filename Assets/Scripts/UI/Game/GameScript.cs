e54b863c0777e0645b36928a5e8a65f90cd52d41
Nikola Jokovic <jokovic.nikola@gmail.com>
2021-06-23 14:48:47 +0200

    root last

--- Assets/Scripts/UI/Game/GameScript.cs
+++ Assets/Scripts/UI/Game/GameScript.cs
@@ -363,8 +363,8 @@ public class GameScript : MonoBehaviourPunCallbacks
             Debug.Log("karta:" + canvacesOfFirstDeck.transform.parent.parent.parent.name);
             Debug.Log("karta 4:" + canvacesOfFirstDeck.transform.parent.parent.parent.parent.name);
             Debug.Log("karta:" + transform.name);
-            Debug.Log("karta:" + canvacesOfFirstDeck.transform.parent.parent.parent.parent.parent.parent.name);
-            var card =  canvacesOfFirstDeck.transform.parent.parent.parent.parent.parent.parent.Find($"{obj}(Clone)").gameObject;
+           // Debug.Log("karta:" + canvacesOfFirstDeck.transform.parent.parent.parent.parent.parent.parent.name);
+            var card =  canvacesOfFirstDeck.transform.root.Find($"{obj}(Clone)").gameObject;
             Debug.Log("card:" + card.name);
             //var prefabs = Resources.Load("Prefabs/CardPrefabsStartSVG/"+obj);
             //GameObject gameObj = (GameObject)prefabs;
