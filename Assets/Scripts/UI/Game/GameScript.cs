c13e014bb3cf6ee6798e561349e1603f982c2a06
Nikola Jokovic <jokovic.nikola@gmail.com>
2021-06-25 11:11:31 +0200

    new correction on showing the images of player.

--- Assets/Scripts/UI/Game/GameScript.cs
+++ Assets/Scripts/UI/Game/GameScript.cs
@@ -433,7 +433,42 @@ public class GameScript : MonoBehaviourPunCallbacks
 
                         ThirdPlayerName.text = PhotonNetwork.CurrentRoom.GetPlayer(vv.Key).NickName;
                         Dictionary<int, Player> valuePlayers = PhotonNetwork.CurrentRoom.Players;
+                        int interation = 0;
+                        foreach(var player in valuePlayers)
+                        {
+                            if (!PhotonNetwork.LocalPlayer.NickName.Equals(PhotonNetwork.CurrentRoom.GetPlayer(player.Key).NickName))
+                            {
+                                if (PhotonNetwork.CurrentRoom.GetPlayer(vv.Key).Equals(PhotonNetwork.CurrentRoom.GetPlayer(player.Key)))
+                                {
+                                    if (interation == 0)
+                                    {
+                                        Texture2D tex2 = new Texture2D(83, 87);
+                                        byte[] valuePicture2 = (byte[])PhotonNetwork.CurrentRoom.GetPlayer(vv.Key).CustomProperties["Picture"];
+                                        tex2.LoadImage(valuePicture2);
+                                        // Assign texture to renderer's material.
+                                        //GetComponent<Renderer>().material.mainTexture = tex;
+                                        UnityEngine.UI.Image ProfilePic2 = FirstPlayerImage.GetComponent<UnityEngine.UI.Image>();
+                                        ProfilePic2.sprite = Sprite.Create(tex2, new Rect(0, 0, 83, 87), new Vector2());
 
+                                        FirstPlayerName.text = PhotonNetwork.CurrentRoom.GetPlayer(vv.Key).NickName;
+                                    }
+                                    else
+                                    {
+                                        Texture2D tex3 = new Texture2D(83, 87);
+                                        byte[] valuePicture3 = (byte[])PhotonNetwork.CurrentRoom.GetPlayer(vv.Key).CustomProperties["Picture"];
+                                        tex3.LoadImage(valuePicture3);
+                                        // Assign texture to renderer's material.
+                                        //GetComponent<Renderer>().material.mainTexture = tex;
+                                        UnityEngine.UI.Image ProfilePic3 = SecondPlayerImage.GetComponent<UnityEngine.UI.Image>();
+                                        ProfilePic3.sprite = Sprite.Create(tex3, new Rect(0, 0, 83, 87), new Vector2());
+
+                                        SecondPlayerName.text = PhotonNetwork.CurrentRoom.GetPlayer(vv.Key).NickName;
+                                    }
+
+                                    interation++;
+                                }
+                            }
+                        }
                     }
 
                    
@@ -486,6 +521,7 @@ public class GameScript : MonoBehaviourPunCallbacks
                 }
                 tempI++;
             }
+           
         }
     }
 
